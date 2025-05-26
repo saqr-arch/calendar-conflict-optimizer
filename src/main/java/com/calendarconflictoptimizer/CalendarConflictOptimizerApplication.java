bd5bf73946688608f02e6763f66e8ad1e0740cea
Hassan Ahmed Hassan Ibrahim <AHI.Hassan@almaviva.it>
2025-05-26 10:42:47 +0300

    Initial commit: calender conflict optimizer setup

---
+++ src/main/java/com/calendarconflictoptimizer/CalendarConflictOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.calendarconflictoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarConflictOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarConflictOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calendarconflictoptimizer/TestCalendarConflictOptimizerApplication.java
@@ -0,0 +1,11 @@
+package com.calendarconflictoptimizer;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCalendarConflictOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CalendarConflictOptimizerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
